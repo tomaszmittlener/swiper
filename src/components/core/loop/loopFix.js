3747c77fca0cca514c3c9e9332829861be3db13d
Tomasz Mittlener <t.mittlener@gmail.com>
2019-06-05 15:16:46 +0200

    feat: add debugging setup

--- src/components/core/loop/loopFix.js
+++ src/components/core/loop/loopFix.js
@@ -1,7 +1,7 @@
 export default function () {
   const swiper = this;
   const {
-    params, activeIndex, slides, loopedSlides, allowSlidePrev, allowSlideNext, snapGrid, rtlTranslate: rtl,
+    params, activeIndex, slides, loopedSlides, allowSlidePrev, allowSlideNext, snapGrid, rtlTranslate: rtl, realIndex,
   } = swiper;
   let newIndex;
   swiper.allowSlidePrev = true;
@@ -13,12 +13,44 @@ export default function () {
 
   // Fix For Negative Oversliding
   if (activeIndex < loopedSlides) {
-    newIndex = slides.length - activeIndex - loopedSlides;
+    // LOGS
+    const { slidesPerGroup, slidesPerView } = swiper.params
+
+    const totalSlides = slides.length - (loopedSlides * 2);
+    const totalSlidesItems = slides.length;
+
+    const TEST_PATTERN = totalSlides % slidesPerView === 0
+
+    console.group();
+
+    console.log('slidesPerGroup', slidesPerGroup);
+    console.log('slidesPerView', slidesPerView);
+    console.log('totalSlides', totalSlides);
+    console.log('slides + duplicates', totalSlidesItems);
+    console.log('loopedSlides', loopedSlides);
+    console.log('TEST_PATTERN', TEST_PATTERN);
+    console.groupEnd()
+
+
+    // FIX for Carousel 1, slidesPerView: 3, slidesPerGroup: 3, slidesAmount: 11
+    // newIndex = slides.length - activeIndex - loopedSlides;
+
+    // OLD FIX
+    newIndex = (slides.length - (loopedSlides * 3)) + activeIndex;
+    newIndex += loopedSlides;
+
+    console.group();
+
+    console.log('activeIndex', activeIndex);
+    console.log('newIndex', newIndex);
+
+    console.groupEnd()
 
     const slideChanged = swiper.slideTo(newIndex, 0, false, true);
     if (slideChanged && diff !== 0) {
       swiper.setTranslate((rtl ? -swiper.translate : swiper.translate) - diff);
     }
+
   } else if ((params.slidesPerView === 'auto' && activeIndex >= loopedSlides * 2) || (activeIndex >= slides.length - loopedSlides)) {
     // Fix For Positive Oversliding
     newIndex = -slides.length + activeIndex + loopedSlides;
